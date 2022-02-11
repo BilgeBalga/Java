b15f4c83a42a29d5470ba09c705ef32b0f3d2041
Bilge Balga <82833137+BilgeBalga@users.noreply.github.com>
2022-02-11 21:40:19 +0300

    Add files via upload

---
+++ datatypes/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int sayi = 12;
+        sayi = 131;
+
+        double  sayi3 = 14.5;
+
+        char karakter = 'A';
+
+        String sehir = "Ankara";
+
+        boolean dogruMu =false;
+
+    }
+}
