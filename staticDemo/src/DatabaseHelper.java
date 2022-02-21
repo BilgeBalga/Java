6a1978ae3fb1ac6f39bd7a31aeee97f4d99d4b84
Bilge Balga <82833137+BilgeBalga@users.noreply.github.com>
2022-02-21 13:48:43 +0300

    Add files via upload

---
+++ staticDemo/src/DatabaseHelper.java
@@ -0,0 +1,17 @@
+public class DatabaseHelper {
+    public static class Crud{ //Crud: Cerate read update delete.
+        public static void delete(){
+
+        }
+        public static void update(){
+
+        }
+
+    }
+
+    public static class Connection{
+        public static void crateConnection(){
+
+        }
+    }
+}
