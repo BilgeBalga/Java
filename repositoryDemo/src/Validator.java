dd0cd885bae3c2970368f066147460478c475148
Bilge Balga <82833137+BilgeBalga@users.noreply.github.com>
2022-03-03 12:25:56 +0300

    Add files via upload

---
+++ repositoryDemo/src/Validator.java
@@ -0,0 +1,8 @@
+public class Validator {
+    public <T extends IEntity> void validate(T entity){ //validater artık generic
+
+    }
+}
+
+
+//Validate = doğrulama = nesnenin kurallara uygun olup olmadığını çözmek için kullanılır
